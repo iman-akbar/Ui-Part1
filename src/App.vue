0ec43525f8e9dcd42739d4875d7f5eef4b1d3562
Iman Akbar <iman.akbar@dwidasa.com>
2021-06-20 11:11:38 +0700

    opacity

--- src/App.vue
+++ src/App.vue
@@ -93,10 +93,10 @@
             <span style="
   font-weight: bold;
   
-  color: #021bf9;">Deffinition</span>; a practice or exercise to test or improve one's fitness
-            for athletic competition, ability, or performance to exhaust
-            (something, such as a mine) by working to devise, arrange, or
-            achieve by resolving difficulties. Merriam-Webster.com Dictionary.
+  color: #021bf9;">Deffinition</span>; a practice or exercise to test or improve one's fitness for
+            athletic competition, ability, or performance to exhaust (something,
+            such as a mine) by working to devise, arrange, or achieve by
+            resolving difficulties. Merriam-Webster.com Dictionary.
           </p>
           <p
             style="
@@ -152,32 +152,24 @@
       </carousel-3d>
       <p class="text-center POV" sty>POV</p>
       <!-- </v-card> -->
-      
+
       <v-row justify="center">
         <p class="Lorem-ipsum-dolor-si">
-          .Lorem-ipsum-dolor-si {
-          width: 619px;
-          height: 135px;
-          margin: 30px 25px 67px 27px;
-          font-family: WorkSans;
-          font-size: 18px;
-          font-weight: normal;
-          font-stretch: normal;
-          font-style: normal;
-          line-height: 1.5;
-          letter-spacing: -0.64px;
-          text-align: center;
-          color: var(--white);
-          }
+          .Lorem-ipsum-dolor-si { width: 619px; height: 135px; margin: 30px 25px
+          67px 27px; font-family: WorkSans; font-size: 18px; font-weight:
+          normal; font-stretch: normal; font-style: normal; line-height: 1.5;
+          letter-spacing: -0.64px; text-align: center; color: var(--white); }
         </p>
       </v-row>
       <v-row justify="center">
         <p class="Resource">Resource</p>
       </v-row>
       <v-row justify="center">
-        <p
-          class="These-cases-are-perf"
-        >These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best</p>
+        <p class="These-cases-are-perf">
+          These cases are perfectly simple and easy to distinguish. In a free
+          hour, when our power of choice is untrammelled and when nothing
+          prevents our being able to do what we like best
+        </p>
       </v-row>
       <v-row justify="center">
         <p class="Help-Tips">Help & Tips</p>
@@ -186,9 +178,11 @@
         <v-col v-for="(tips, i) in tips" :key="i" cols="auto">
           <v-card class="mx-auto" max-width="400">
             <v-img class="white--text align-end" height="200px" :src="tips.image">
-              <v-card-title>
+              <v-card-title class="cardColor">
                 <v-row>
-                  <v-col cols="9"><p style="width: 218px;
+                  <v-col cols="9">
+                    <p
+                      style="width: 218px;
   height: 38px;
   margin: 0 16px 0 0;
   font-family: WorkSans;
@@ -198,7 +192,9 @@
   font-style: normal;
   line-height: normal;
   letter-spacing: -0.57px;
-  color: white;">{{tips.title}}</p></v-col>
+  color: white;"
+                    >{{ tips.title }}</p>
+                  </v-col>
                   <v-col>
                     <v-btn fab>
                       <v-img
@@ -226,8 +222,8 @@
   height: 158px;
   margin: 0 0 184px 212px;"
       ></v-img>-->
-      
-      <v-row justify="center" align="center">       
+
+      <v-row justify="center" align="center">
         <v-col cols="auto">
           <p
             style="width: 619px;
@@ -242,41 +238,39 @@
   letter-spacing: -0.64px;
   text-align: center;
   color:white;"
-          >The wise man therefore always holds in these matters to this principle of selection.</p>
+          >
+            The wise man therefore always holds in these matters to this
+            principle of selection.
+          </p>
         </v-col>
       </v-row>
       <div>
         <v-img
-            
-            contain
-            
-            src="./assets/group-3.png"
-            srcset="./assets/group-3@2x.png 2x, ./assets/group-3@3x.png 3x"
-            style="width: 159px;
-  height: 158px;
-  margin: 127px 71px 0 0;
-  "
-          ></v-img>
+          contain
+          src="./assets/group-3.png"
+          srcset="./assets/group-3@2x.png 2x, ./assets/group-3@3x.png 3x"
+          style="width: 159px;
+                height: 158px;
+                margin: 127px 71px 0 0;
+                "
+        ></v-img>
       </div>
-      <v-toolbar
-      color="#021bf9"
-    >
-      
-
-      <v-toolbar-title class="ml-15 xs bottomTool" style="width: 98px;
-  height: 19px;
-  margin: 2px 716px 3px 0;
-  font-family: WorkSans;
-  font-size: 16px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: white;">wknd@2020</v-toolbar-title>
-
-     
-    </v-toolbar>
+      <v-toolbar color="#021bf9">
+        <v-toolbar-title
+          class="ml-15 xs bottomTool"
+          style="width: 98px;
+                height: 19px;
+                margin: 2px 716px 3px 0;
+                font-family: WorkSans;
+                font-size: 16px;
+                font-weight: bold;
+                font-stretch: normal;
+                font-style: normal;
+                line-height: normal;
+                letter-spacing: normal;
+                color: white;"
+        >wknd@2020</v-toolbar-title>
+      </v-toolbar>
     </v-container>
   </v-app>
 </template>
@@ -452,12 +446,16 @@ export default {
   margin: 3px 0 3px 16px;
   object-fit: contain;
 }
-.bottomTool{
-  margin-left:500px;
+.bottomTool {
+  margin-left: 500px;
 }
-.trial{
+.trial {
   position: absolute;
   bottom: 5px;
   left: 10px;
 }
+.cardColor {
+   background-color: rgba(0, 0, 0, 0.51);
+   
+ }
 </style>
